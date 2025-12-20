828ad20ff10900c896c0922c92eda3a24c5b4a84
JustBronwKid <larakid8080@gmail.com>
2025-12-20 21:43:19 +0630

    FIx

--- apex-play-backend/src/main.ts
+++ apex-play-backend/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 8000, '0.0.0.0');
 }
 bootstrap();
